ec1b7a1a5f7894f4d123aa92ddd2917a5533b3e3
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-05 23:46:03 -0500

    Update controller-install.sh

--- controller-install.sh
+++ controller-install.sh
@@ -14,14 +14,17 @@ sudo apt-get update -y
 sudo apt-get install gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake -y
 sudo apt-get install llvm-dev llvm clang libclang-dev libclang-cpp-dev libedit-dev -y
 # Main Install
+if [ ! -f chapel-1.29.0.tar.gz]; then
 wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
 tar xzf chapel-1.29.0.tar.gz
-cd chapel-1.29.0
+fi
+cd $HOME/chapel-1.29.0
 source util/setchplenv.bash
 ./configure
 sudo make install
 ./util/printchplenv
 export CHPL_COMM=gasnet
+cd $CHPL_HOME
 sudo make
 chpl -o hello $CHPL_HOME/examples/hello6-taskpar-dist.chpl
 export GASNET_SPAWNFN=S
