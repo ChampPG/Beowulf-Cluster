2cd44296010ed40c95f8d2137b2f1a6ac8a42708
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-05 22:14:58 -0500

    Create controller-install.sh

---
+++ controller-install.sh
@@ -0,0 +1,22 @@
+# SSH-Keys
+if [ ! -f $HOME/.ssh/id_rsa ]; then
+  ssh-keygen -q
+fi
+
+# Set hostname
+if [ $HOSTNAME != controller ]; then
+  sudo hostnamectl set-hostname controller
+fi
+
+#Chapel Setup
+wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
+tar xzf chapel-1.29.0.tar.gz
+cd chapel-1.29.0
+source util/setchplenv.bash
+sudo make
+./util/printchplenv
+
+# IP Address
+echo "Subnet #: "  
+read subnet  
+ip addr | grep /$subnet | awk {'print $1'}
