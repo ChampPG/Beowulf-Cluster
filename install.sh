39e7395236ca8607648fdf9aeed1a36a288fd721
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-05 21:49:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # For Linux bash:
 wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
-tar xzf chapel-1.18.0.tar.gz
-cd chapel-1.18.0
+tar xzf chapel-1.29.0.tar.gz
+cd chapel-1.29.0
 source util/setchplenv.bash
 sudo make
 ./util/printchplenv
