0b39cd129120c402537b076893eee981cfb7d888
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-05 22:21:21 -0500

    Update controller-install.sh

--- controller-install.sh
+++ controller-install.sh
@@ -15,6 +15,12 @@ cd chapel-1.29.0
 source util/setchplenv.bash
 sudo make
 ./util/printchplenv
+export CHPL_COMM=gasnet
+sudo make
+chpl -o hello $CHPL_HOME/examples/hello6-taskpar-dist.chpl
+export GASNET_SPAWNFN=S
+
+echo 'export GASNET_SSH_SERVERS="host1 host2 host3 ..."'
 
 # IP Address
 echo "Subnet #: "  
