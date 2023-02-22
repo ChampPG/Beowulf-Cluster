e56d6fef12caaf425859b55456894486b1120646
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-22 18:41:13 -0500

    Update controller-install.sh

--- controller-install.sh
+++ controller-install.sh
@@ -1,39 +1,61 @@
-# SSH-Keys
-if [ ! -f $HOME/.ssh/id_rsa ]; then
-  ssh-keygen
-fi
+if [ $0 = "yes" ]; then
 
-# Set hostname
-if [ "$HOSTNAME" != controller ]; then
-  sudo hostnamectl set-hostname controller
-fi
+  # SSH-Keys
+  if [ ! -f $HOME/.ssh/id_rsa ]; then
+    ssh-keygen
+  fi
 
-#Chapel Setup
-# Prerequisites
-sudo apt-get update -y
-sudo apt-get install gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake -y
-sudo apt-get install llvm-dev llvm clang libclang-dev libclang-cpp-dev libedit-dev -y
+  # Set hostname
+  if [ "$HOSTNAME" != controller ]; then
+    sudo hostnamectl set-hostname controller
+  fi
 
-# Main Install
-if [ ! -f $HOME/chapel-1.29.0.tar.gz]; then
-  cd $HOME
-  wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
-  tar xzf chapel-1.29.0.tar.gz
-fi
+  #Chapel Setup
+  # Prerequisites
+  sudo apt-get update -y
+  sudo apt-get install gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake -y
+  sudo apt-get install llvm-dev llvm clang libclang-dev libclang-cpp-dev libedit-dev -y
 
-cd $HOME/chapel-1.29.0
-source util/setchplenv.bash
-sudo make
+  # Main Install
+  if [ ! -f $HOME/chapel-1.29.0.tar.gz]; then
+    cd $HOME
+    wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
+    tar xzf chapel-1.29.0.tar.gz
+  fi
 
-#export CHPL_COMM=gasnet
-#cd $CHPL_HOME
-#sudo make
-#chpl -o hello $CHPL_HOME/examples/hello6-taskpar-dist.chpl
-#export GASNET_SPAWNFN=S
-#echo 'export GASNET_SSH_SERVERS="host1 host2 host3 ..."'
-#echo './hello -nl 2'
+  cd $HOME/chapel-1.29.0
+  source util/setchplenv.bash
+  sudo make
 
-# IP Address
-echo "Subnet #: "  
-read subnet  
-ip addr | grep /$subnet | awk {'print $2'}
+  export CHPL_COMM=gasnet
+  sudo make
+
+  export GASNET_SPAWNFN=S:
+  export GASNET_SSH_SERVERS="beowulf01 beowulf02 beowulf03 beowulf04 beowulf05":
+
+  ./hello -nl 5
+
+  #export CHPL_COMM=gasnet
+  #cd $CHPL_HOME
+  #sudo make
+  #chpl -o hello $CHPL_HOME/examples/hello6-taskpar-dist.chpl
+  #export GASNET_SPAWNFN=S
+  #echo 'export GASNET_SSH_SERVERS="host1 host2 host3 ..."'
+  #echo './hello -nl 2'
+
+  # IP Address
+  echo "Subnet #: "  
+  read subnet  
+  ip addr | grep /$subnet | awk {'print $2'}
+elif [ $0 = "no" ]; then
+
+  cd $HOME/chapel-1.29.0
+  
+  export CHPL_COMM=gasnet
+  sudo make
+
+  export GASNET_SPAWNFN=S:
+  export GASNET_SSH_SERVERS="beowulf01 beowulf02 beowulf03 beowulf04 beowulf05":
+else
+  echo "usage is wrong ./controller-install [yes|no]"
+  
