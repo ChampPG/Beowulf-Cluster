ce53b02217951d041c343fcd643c380256128b66
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-22 18:49:19 -0500

    Update client-install.sh

--- client-install.sh
+++ client-install.sh
@@ -1,35 +1,43 @@
-# SSH-Keys
-if [ ! -f $HOME/.ssh/id_rsa ]; then
-  ssh-keygen
-fi
+if [ $0 = "yes" ]; then
+  # SSH-Keys
+  if [ ! -f $HOME/.ssh/id_rsa ]; then
+    ssh-keygen
+  fi
 
-# Set hostname
-echo "Node #: "  
-read node
-if [ "$HOSTNAME" != node$node ]; then
-  sudo hostnamectl set-hostname node$node
-fi
+  # Set hostname
+  echo "Node #: "  
+  read node
+  if [ "$HOSTNAME" != node$node ]; then
+    sudo hostnamectl set-hostname node$node
+  fi
 
-# Prerequisites
-sudo apt-get update -y
-sudo apt-get install gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake -y
-sudo apt-get install llvm-dev llvm clang libclang-dev libclang-cpp-dev libedit-dev -y
+  # Prerequisites
+  sudo apt-get update -y
+  sudo apt-get install gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-config cmake -y
+  sudo apt-get install llvm-dev llvm clang libclang-dev libclang-cpp-dev libedit-dev -y
 
-#Chapel Setup
-if [ ! -f $HOME/chapel-1.29.0.tar.gz]; then
-  wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
-  tar xzf chapel-1.29.0.tar.gz
-fi
+  #Chapel Setup
+  if [ ! -f $HOME/chapel-1.29.0.tar.gz ]; then
+    wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
+    tar xzf chapel-1.29.0.tar.gz
+  fi
 
-cd chapel-1.29.0
-source util/setchplenv.bash
-sudo make
-./util/printchplenv
+  cd chapel-1.29.0
+  source util/setchplenv.bash
+  sudo make
+  ./util/printchplenv
 
-export CHPL_COMM=gasnet:
-sudo make
+  export CHPL_COMM=gasnet
+  sudo make
 
-# IP Address
-echo "Subnet #: "  
-read subnet  
-ip addr | grep /$subnet | awk {'print $1'}
+  # IP Address
+  echo "Subnet #: "  
+  read subnet  
+  ip addr | grep /$subnet | awk {'print $1'}
+ elif [ $0 = "no" ]; then
+ 
+   export CHPL_COMM=gasnet
+   sudo make
+   
+ else
+  echo "wrong usage not ./client-installer [yes|no]"
