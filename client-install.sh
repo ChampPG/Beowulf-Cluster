0e781e6b5ef060262411ade96f2ca48f56cd1216
ChampPG <71086240+ChampPG@users.noreply.github.com>
2023-02-22 18:34:39 -0500

    Update client-install.sh

--- client-install.sh
+++ client-install.sh
@@ -16,8 +16,11 @@ sudo apt-get install gcc g++ m4 perl python3 python3-dev bash make mawk git pkg-
 sudo apt-get install llvm-dev llvm clang libclang-dev libclang-cpp-dev libedit-dev -y
 
 #Chapel Setup
-wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
-tar xzf chapel-1.29.0.tar.gz
+if [ ! -f $HOME/chapel-1.29.0.tar.gz]; then
+  wget https://github.com/chapel-lang/chapel/releases/download/1.29.0/chapel-1.29.0.tar.gz
+  tar xzf chapel-1.29.0.tar.gz
+fi
+
 cd chapel-1.29.0
 source util/setchplenv.bash
 sudo make
